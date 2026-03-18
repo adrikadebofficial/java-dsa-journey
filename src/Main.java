6f4b36217e9662a4a437f2f675ae190f64568702
Adrika Deb <adrika.deb.official@gmail.com>
2026-03-18 13:53:36 +0530

    Add FizzBuzz solution using for loop and modulo operator

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,27 @@
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
-        LocalDate today = LocalDate.now();
+        // FizzBuzz program
+        /*Loop through numbers 1 to 100
+        Print "Fizz" if the number is divisible by 3
+        Print "Buzz" if the number is divisible by 5
+        Print "FizzBuzz" if it's divisible by both 3 and 5
+        Otherwise, just print the number itself
+        * */
 
-        DateTimeFormatter datetimeformat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        String formattedDate = today.format(datetimeformat);
+        for(int i = 1; i <= 100; i++){
+            if(i%3 == 0 && i%5 == 0){
+                System.out.println("FizzBuzz");
+            }else if(i% 3 == 0){
+                System.out.println("Fizz");
+            }else if(i%5 == 0){
+                System.out.println("Buzz");
+            }else{
+                System.out.println("Number is : " + i );
+            }
 
-        System.out.println("Hello World, Name: Adrika, Date: " + formattedDate );
+        }
+
 
     }
 }
